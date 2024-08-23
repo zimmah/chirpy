4e1a268a1e4b0a038b828ad31a0d37f41501bf87
zimmah <zimmah@gmail.com>
2024-08-23 16:12:34 -0500

    add metrics

--- main.go
+++ main.go
@@ -6,19 +6,46 @@ import (
 )
 
 const port = "8080"
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, private")
+		cfg.fileserverHits++
+		fmt.Println("Request received")
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	serverMux := http.NewServeMux()
+	config := apiConfig{}
+
+	
 
 	serverMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Content-Type", "text/plain; charset=utf-8")
         w.WriteHeader(http.StatusOK)
         w.Write([]byte("OK"))
     })
+
+	serverMux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte(fmt.Sprintf("Hits: %d", config.fileserverHits)))
+    })
+
+	serverMux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+        w.WriteHeader(http.StatusOK)
+        config.fileserverHits = 0
+    })
 	
 	fileServer := http.FileServer(http.Dir("."))
 	
-	serverMux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	serverMux.Handle("/app/", config.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
 
 	server := &http.Server{
 		Addr: ":" + port,
