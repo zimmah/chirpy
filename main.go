0fdd07bb34a1aabc41ff9708ca6fe37c83c9e38a
zimmah <zimmah@gmail.com>
2024-08-24 14:19:27 -0500

    add debug flag

--- main.go
+++ main.go
@@ -3,12 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
+	"flag"
 
 	"github.com/zimmah/chirpy/internal/database"
 	"github.com/zimmah/chirpy/internal/router"
 )
 
 func main() {
+	debug := flag.Bool("debug", false, "Enable debug mode")
+	flag.Parse()
+
+	if *debug {
+		fmt.Println("Debug mode enabled. Deleting database.json...")
+
+		err := os.Remove("database.json")
+		if err != nil {
+			fmt.Printf("Error deleting file: %v\n", err)
+		} else {
+			fmt.Println("database.json deleted succesfully.")
+		}
+	}
 	_, err := database.NewDB("./database.json")
 	if err != nil {
 		fmt.Println("Error creating database")
